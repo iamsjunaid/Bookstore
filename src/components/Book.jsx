69bf39e382927cb5f0674d5f35d0742516a1c0c0
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-15 15:47:19 +0530

    create book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,33 @@
+import './styles/Book.css';
+
 const Book = () => (
-  <div>
-    <h1>Book</h1>
+  <div className="book">
+    <article>
+      <div className="book-details">
+        <p>Book.Genre</p>
+        <p>Book.Title</p>
+        <p>Book.Author</p>
+      </div>
+      <div className="interactions">
+        <button type="submit">Comments</button>
+        <button type="submit">Remove</button>
+        <button type="submit">Edit</button>
+      </div>
+    </article>
+    <article>
+      <div>circle</div>
+      <div className="progress-text">
+        <p>Book.progress</p>
+        <p>Completed</p>
+      </div>
+    </article>
+    <article>
+      <div>
+        <p>CURRENT CHAPTER</p>
+        <p>Book.chapter</p>
+      </div>
+      <button type="submit">UPDATE PROGRESS</button>
+    </article>
   </div>
 );
 
