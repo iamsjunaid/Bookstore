5e80e9a0f91aa65dbeafa9c76774c9203ffdc9b5
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-19 15:15:01 +0530

    add initialState in booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,23 @@
 const ADD_BOOK = 'books/ADD_BOOK';
 const REMOVE_BOOK = 'books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'Code Complete',
+    author: 'Steve McConnell',
+  },
+  {
+    id: 2,
+    title: 'Programming Pearls',
+    author: 'Jon Bentley',
+  },
+  {
+    id: 3,
+    title: 'Cacking the Coding Interview',
+    author: 'Gayle Laakmann',
+  },
+];
 
 export const addBook = (book) => ({
   type: ADD_BOOK,
@@ -15,10 +31,10 @@ export const removeBook = (id) => ({
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_BOOK':
+    case ADD_BOOK:
       return [...state, action.payload];
 
-    case 'REMOVE_BOOK':
+    case REMOVE_BOOK:
       return [...state, state.filter((book) => book.id !== action.payload)];
 
     default:
