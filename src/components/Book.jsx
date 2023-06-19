2212e458fd99a35b93c340882e1ae97e8c097597
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-19 21:56:06 +0530

    Update Book.jsx

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,34 +1,50 @@
+import { useSelector, useDispatch } from 'react-redux';
 import './styles/Book.css';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = () => (
-  <div className="book">
-    <article>
-      <div className="book-details">
-        <p>Book.Genre</p>
-        <p>Book.Title</p>
-        <p>Book.Author</p>
-      </div>
-      <div className="interactions">
-        <button type="submit">Comments</button>
-        <button type="submit">Remove</button>
-        <button type="submit">Edit</button>
-      </div>
-    </article>
-    <article>
-      <div>circle</div>
-      <div className="progress-text">
-        <p>Book.progress</p>
-        <p>Completed</p>
-      </div>
-    </article>
-    <article>
-      <div>
-        <p>CURRENT CHAPTER</p>
-        <p>Book.chapter</p>
-      </div>
-      <button type="submit">UPDATE PROGRESS</button>
-    </article>
-  </div>
-);
+const Book = () => {
+  const booksList = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
+  const removeBookFromList = (id) => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <div>
+
+      {booksList.map((book) => (
+        <div key={book.id} className="book">
+          <article>
+            <div className="book-details">
+              <p>Book.Genre</p>
+              <p>{book.id}</p>
+              <p>{book.title}</p>
+              <p>{book.author}</p>
+            </div>
+            <div className="interactions">
+              <button type="submit">Comments</button>
+              <button type="submit" onClick={() => removeBookFromList(book.id)}>Remove</button>
+              <button type="submit">Edit</button>
+            </div>
+          </article>
+          <article>
+            <div>circle</div>
+            <div className="progress-text">
+              <p>Book.progress</p>
+              <p>Completed</p>
+            </div>
+          </article>
+          <article>
+            <div>
+              <p>CURRENT CHAPTER</p>
+              <p>Book.chapter</p>
+            </div>
+            <button type="submit">UPDATE PROGRESS</button>
+          </article>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Book;
