635b7ebcf3384a91b866ea21b84622f00769a064
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-20 21:05:29 +0530

    create store and bookSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,40 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const initialState = [
-  {
-    id: 1,
-    title: 'Code Complete',
-    author: 'Steve McConnell',
-    category: 'Programming',
-  },
-  {
-    id: 2,
-    title: 'Programming Pearls',
-    author: 'Jon Bentley',
-    category: 'Programming',
-  },
-  {
-    id: 3,
-    title: 'Cacking the Coding Interview',
-    author: 'Gayle Laakmann',
-    category: 'Programming',
-  },
-];
+const getBooksURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/wUxXlMuiwuovrvJw2spJ/books';
+
+// Action
+export const getBooks = createAsyncThunk('getBooks', async (thunkAPI) => {
+  try {
+    const response = await fetch(getBooksURL);
+    return response.json();
+  } catch (error) {
+    return thunkAPI.rejectWithValue({ error: error.message });
+  }
+});
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState,
+  initialState: {
+    isLoading: false,
+    data: null,
+    isError: false,
+  },
   reducers: {
-    addBook: (state, action) => {
-      const newBook = { ...action.payload, id: state.length + 1 };
-      state.push(newBook);
-    },
+    addBook: (state, action) => [...state, action.book],
     removeBook: (state, action) => {
-      const bookIndex = state.findIndex((book) => book.id === action.payload);
-      if (bookIndex !== -1) {
-        state.splice(bookIndex, 1);
-      }
+      const filteredBooks = state.filter((book) => book.item_id !== action.item_id);
+      const updatedArray = filteredBooks.map((book, index) => ({ ...book, item_id: index + 1 }));
+      return [...updatedArray];
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBooks.pending, (state) => {
+        state.isLoading = true; // eslint-disable-line no-param-reassign
+      })
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.isLoading = false; // eslint-disable-line no-param-reassign
+        state.data = action.payload; // eslint-disable-line no-param-reassign
+      })
+      .addCase(getBooks.rejected, (state) => {
+        console.log('rejected');
+        state.isLoading = false; // eslint-disable-line no-param-reassign
+        state.isError = true; // eslint-disable-line no-param-reassign
+      });
   },
 });
 
