07594c5ff118c70f90dd3baf99d9ac1f59f7c6b0
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-21 19:56:47 +0530

    Update Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,10 +1,12 @@
 import AddBook from './AddBook';
 import Book from './Book';
+import HorizontalBar from './HorizontalBar';
 import './styles/Books.css';
 
 const Books = () => (
   <div className="books">
     <Book />
+    <HorizontalBar />
     <AddBook />
   </div>
 );
