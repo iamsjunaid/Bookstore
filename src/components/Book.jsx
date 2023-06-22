a7ef2f5733e738e92a384b390bb633a494b8a411
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-22 13:13:47 +0530

    add space between chapter and chapter number

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -74,7 +74,10 @@ const Book = () => {
           <article className="container-4">
             <div>
               <p className="current-chapter">CURRENT CHAPTER</p>
-              <p className="chapter">{Math.floor(Math.random() * 10)}</p>
+              <p className="chapter">
+                Chapter&nbsp;
+                {Math.floor(Math.random() * 10)}
+              </p>
             </div>
             <button type="submit" className="btn-submit"><span>UPDATE PROGRESS</span></button>
           </article>
